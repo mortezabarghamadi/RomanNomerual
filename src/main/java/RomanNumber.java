21505abe5fcfa36d5df3f0d5585efc187669d80e
mortezabarghamadi <mortezabarghamadi5@gmail.com>
2023-07-17 12:39:23 +0330

    ruman numerual

--- src/main/java/RomanNumber.java
+++ src/main/java/RomanNumber.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class RomanNumber {
-}
+public class RomanNumber {
+    public String convert(int arabicnumber) {
+        StringBuilder romanNumber= new StringBuilder();
+        if(arabicnumber>=900){
+            romanNumber.append("CM");
+            arabicnumber-=900;
+        }
+        if (arabicnumber>=500) {
+        romanNumber.append("D");
+        arabicnumber-=500;
+        }
+        if(arabicnumber>=400){
+            romanNumber.append("CD");
+            arabicnumber-=400;
+        }
+        while (arabicnumber>=100){
+            romanNumber.append("C");
+            arabicnumber-=100;
+        }
+        if(arabicnumber>=90){
+            romanNumber.append("XC");
+            arabicnumber-=90;
+        }
+        if (arabicnumber>=50){
+            romanNumber.append("L");
+            arabicnumber-=50;
+        }
+        if(arabicnumber>=40){
+            romanNumber.append("XL");
+            arabicnumber-=40;
+        }
+        while (arabicnumber>=10){
+            romanNumber.append("X");
+            arabicnumber-=10;
+        }
+        if (arabicnumber==9) {
+            romanNumber.append("IX");
+            arabicnumber -= 9;
+        }
+        if (arabicnumber>=5) {
+            romanNumber.append("V");
+            arabicnumber-=5;
+        }
+        if(arabicnumber==4){
+            romanNumber.append("IV");
+            arabicnumber-=4;
+        }
+        for ( int index=0;index<arabicnumber;index++){
+            romanNumber.append("I");
+        }
+        return romanNumber.toString();
+    }}
--- src/test/java/RomanNumberTest.java
+++ src/test/java/RomanNumberTest.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class RomanNumberTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RomanNumberTest {
+
+
+    @Test
+    void Addad_Romi_Chap_konad() {
+            RomanNumber romanNumber = new RomanNumber();
+
+        //    String result=romanNumber.convert( 1);
+        /*assertThat(romanNumber.convert(1)).isEqualTo("I");
+        assertThat(romanNumber.convert(2)).isEqualTo("II");
+        assertThat(romanNumber.convert(3)).isEqualTo("III");
+        assertThat(romanNumber.convert(5)).isEqualTo("V");
+        assertThat(romanNumber.convert(6)).isEqualTo("VI");
+        assertThat(romanNumber.convert(7)).isEqualTo("VII");
+        assertThat(romanNumber.convert(8)).isEqualTo("VIII");
+        assertThat(romanNumber.convert(10)).isEqualTo("X");
+        assertThat(romanNumber.convert(11)).isEqualTo("XI");
+        assertThat(romanNumber.convert(12)).isEqualTo("XII");
+        assertThat(romanNumber.convert(13)).isEqualTo("XIII");
+        assertThat(romanNumber.convert(20)).isEqualTo("XX");
+        assertThat(romanNumber.convert(37)).isEqualTo("XXXVII");
+        assertThat(romanNumber.convert(50)).isEqualTo("L");
+        assertThat(romanNumber.convert(60)).isEqualTo("LX");
+        assertThat(romanNumber.convert(70)).isEqualTo("LXX");
+        assertThat(romanNumber.convert(80)).isEqualTo("LXXX");*/
+        assertThat(romanNumber.convert(949)).isEqualTo("CMXLIX");
+       // assertThat(romanNumber.convert(878)).isEqualTo("DCCCLXXVIII");
+
+
+    }
 }
